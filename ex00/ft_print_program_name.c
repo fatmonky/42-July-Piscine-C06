a158cf81409af49ec237d6c0ca0f00a4936b16f9
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-09 09:29:56 +0800

    complete ex00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 09:06:26 by pteh              #+#    #+#             */
+/*   Updated: 2023/08/09 09:28:20 by pteh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_program_name(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		ft_print_program_name(argv[0]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
