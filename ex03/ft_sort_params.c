445f1a055dd12264db9601d3d6a1acc51f171b2d
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-13 13:58:51 +0800

    completed ex03

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,23 +6,18 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 15:58:05 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/12 17:10:03 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/13 13:57:40 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_print_params(char *str)
 {
 	while (*str)
 	{
-		ft_putchar(*str);
+		write(1, str, 1);
 		str++;
 	}
 }
@@ -37,36 +32,50 @@ int	ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
-void	ft_swap(char *s1, char *s2)
+void	ft_swap(char **s1, char **s2)
 {
-	char *temp;
+	char	*temp;
 
-	temp = s1;
-	s1 = s2;
-	s2 = temp;
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+void	ft_print_args(int argc, char **argv)
+{
+	int	cursor;
+
+	cursor = 1;
+	while (cursor < argc)
+	{
+		ft_print_params(argv[cursor]);
+		write(1, "\n", 1);
+		cursor++;
+	}
 }
 
 int	main(int argc, char **argv)
-{// talking to Nat, what I need is:
- // - inner if, to compare two strings x & y, and to swap if they are smaller.
- // - outer while loop to iterate y
- // - outer outer while loop to iterate x.
- // - comparison string should also be comparing char**. 
+{
 	int	cursor;
 	int	comparator;
 
-	cursor = 1; //this is the first string, which should always be the smallest. 
-	while (cursor < argc - 1)
+	if (argc > 2)
 	{
-		while (comparator < argc - 1) //what's the condition here?
+		cursor = 1;
+		comparator = 1;
+		while (cursor < argc)
 		{
-			if (ft_strcmp(argv[cursor], argv[cursor]) > 0)
+			while (comparator < argc)
 			{
-				ft_swap(argv[cursor], argv[comparator]);
+				if (ft_strcmp(argv[cursor], argv[comparator]) > 0)
+					ft_swap(&argv[cursor], &argv[comparator]);
+				comparator++;
 			}
+			if (comparator == argc)
+				comparator = 1;
+			cursor++;
 		}
-		ft_print_params(argv[cursor]);
-		ft_putchar('\n');
-		cursor++;
 	}
+	ft_print_args(argc, argv);
+	return (0);
 }
